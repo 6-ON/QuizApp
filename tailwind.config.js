96a2eeacfe3d79f571decff1a0e22d3c5c64b075
6-ON <aminhatim6@gmail.com>
2023-01-25 20:44:28 +0100

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,vue}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}

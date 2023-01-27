1e96341ad5b79d965526a8b548e7181fd5b671b1
6-ON <aminhatim6@gmail.com>
2023-01-27 22:06:50 +0100

    adding some components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,35 @@
-<template>
-    <h1>Home</h1>
-</template>
-<script>
+
+<script >
+export default {
+  name:'Home',
+  data(){
+    return {
+      styles:['bg-green-400','bg-blue-400'],
+      current_color :'bg-red-400'
+    }
+  },
+  methods:{
+    updateDiv(style){
+        this.current_color=style
+    },
+    resetColor(){
+      this.current_color = 'bg-red-400'
+    },
+  }
+}
 </script>
+
+<template>
+
+  <div @mouseover="resetColor" class="w-24 h-24" :class="current_color">
+  </div>
+
+  <div class="mt-6 flex gap-2">
+    <div v-for="style in styles" :class="style" class="w-12 h-12 rounded-full" @mouseover="updateDiv(style)" ></div>
+
+  </div>
+
+
+</template>
+
+
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,36 @@
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      logo:'Quiz',
+      inputs: ['email', 'password']
+    }
+  },
+  computed :{
+    alogo(){
+      return this.logo + ' ' + 'App'
+    }
+  }
+}
+</script>
+
 <template>
-    <h1>Login</h1>
+  <div class="main-wrapper">
+    <div class="backdrop-blur rounded-xl p-6">
+      <div>
+        {{ alogo }}
+      </div>
+      <form action="" class="flex flex-col gap-2">
+        <input v-for="input in inputs" :type="input" name="" id="">
+      </form>
+    </div>
+  </div>
 </template>
+<style scoped>
+.main-wrapper {
+  @apply h-screen w-screen flex items-center justify-center bg-cover bg-[url('https://images.pexels.com/photos/289737/pexels-photo-289737.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')]
+}
+
+
+</style>

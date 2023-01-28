e3dc98e10d2049479a4d4f8bc0aa1a0468b700d1
6-ON <aminhatim6@gmail.com>
2023-01-29 00:35:54 +0100

    vue training commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,28 +4,33 @@ export default {
   name:'Home',
   data(){
     return {
-      styles:['bg-green-400','bg-blue-400'],
-      current_color :'bg-red-400'
+      styles:[{name:'red',color:'bg-red-400'},{name:'green',color:'bg-green-400'},{name:'blue',color:'bg-blue-400'}],
+      current_color :0
     }
   },
   methods:{
-    updateDiv(style){
-        this.current_color=style
+    updateDiv(index){
+        this.current_color = index
     },
     resetColor(){
-      this.current_color = 'bg-red-400'
+      this.current_color = 0
     },
+  },
+  computed:{
+    color(){
+      return this.styles[this.current_color]
+    }
   }
 }
 </script>
 
 <template>
 
-  <div @mouseover="resetColor" class="w-24 h-24" :class="current_color">
+  <div class="w-24 h-24" :class="color.color">
   </div>
-
+  <p>{{color.name}}</p>
   <div class="mt-6 flex gap-2">
-    <div v-for="style in styles" :class="style" class="w-12 h-12 rounded-full" @mouseover="updateDiv(style)" ></div>
+    <div v-for="(style,index) in styles" :class="style.color" class="w-12 h-12 rounded-full" @mouseover="updateDiv(index)" ></div>
 
   </div>
 
